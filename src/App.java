08f2d1d67c82605fcd0755f52f194f3461c883b3
Davi <mdavi6085@gmail.com>
2024-11-04 16:43:54 -0300

    tarefa calculo média do módulo 008

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        Media media = new Media();
+
+        System.out.println("O valor da sua média é: " + media.calcularMedia());
+    }
+}
---
+++ src/Media.java
@@ -0,0 +1,10 @@
+public class Media {
+    private double nota1 = 5.8;
+    private double nota2 = 7.2;
+    private double nota3 = 8.8;
+    private double nota4 = 2.9;
+
+    public double calcularMedia() {
+        return (nota1 + nota2 + nota3 + nota4) / 4;
+    }
+}
